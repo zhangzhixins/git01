3acdb85ae2fbc890be2d07182ad8dc62f2889180
zzx <651093386@qq.com>
2020-08-14 10:00:02 +0800

    新添加Test01.java文件

---
+++ src/main/java/com/hy/git01/web/Test01.java
@@ -0,0 +1,9 @@
+package com.hy.git01.web;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("git 学习");
+        System.out.println("向github远程添加一个新项目");
+        System.out.println("进行协作开发");
+    }
+}
