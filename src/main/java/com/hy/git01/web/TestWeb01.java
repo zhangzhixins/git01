07714ce0c5bf5227449a585f08aa670f0f39be63
zzx <651093386@qq.com>
2020-08-14 11:11:02 +0800

    更爱内容01

--- src/main/java/com/hy/git01/web/TestWeb01.java
+++ src/main/java/com/hy/git01/web/TestWeb01.java
@@ -3,5 +3,6 @@ package com.hy.git01.web;
 public class TestWeb01 {
     public static void main(String[] args) {
         System.out.println("testweb01");
+        System.out.println("01写的");
     }
 }
